003cce80dd779efc25a40a1198bcd4ea52c92f39
YuxuanWang-CHINA <wyx20030601@outlook.com>
2018-08-16 22:12:01 +0800

    hide files

--- configs/mysql_set.php
+++
@@ -1,8 +0,0 @@
-<?php
-	namespace Project\Doing;
-	
-	const MYSQL_SERVER_NAME = 'localhost';
-	const MYSQL_USERNAME = 'project14';
-	const MYSQL_PASSWORD = '154yjf6jf_14';
-	const MYSQL_DATABASE = 'project14';
-?>
